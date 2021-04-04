c1841f5a65b2fd63a62a120b065ea79ff126ced9
Ball <ball@reverseads.com>
2021-04-04 15:29:03 +0700

    Create form

---
+++ API/post_number.php
@@ -0,0 +1,27 @@
+<?php
+    header('Content-Type: application/json');
+    //inport SQL Connection
+    include("sql_connection.php");
+
+    // echo "Skills : " . $_POST['number'];
+    // echo "<br/>";
+    // echo "Skills : " . $_POST['price'];
+    // echo "<br/>";
+    // echo "Skills : " . $_POST['type'];
+    // echo "<br/>";
+    // echo "<hr>";
+
+    $number = $_POST['number'];
+    $price = $_POST['price'];
+    $type = $_POST['type'];
+
+    $arr_data = [$number, $price, $type];
+
+    $sql_add_number = "INSERT INTO number (number, price, type)
+    VALUES ('$number', '$price', '$type')";
+
+    $insert_add_number = $conn->query($sql_add_number);
+
+    echo json_encode($arr_data);
+
+?>
--- index.php
+++ index.php
@@ -3,15 +3,68 @@
 
 <head>
     <title>number_x</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     <script src="./jquery/jquery.min.js"></script>
     <script src="./bootstrap/bootstrap.min.js"></script>
 </head>
 
 <body>
+    <?php include("sql_connection.php"); ?>
+    <div class="jumbotron text-center">
+        <h1>number_x</h1>
+    </div>
 
-    <h1>This is a Heading</h1>
-    <p>This is a paragraph.</p>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-4">
+                <h3>Result</h3>
+                <?php
+                    $sql_list_all = "SELECT * FROM `number`";
+                    $result_list_all = $conn->query($sql_list_all);
+                ?>
+
+                <table border="1" style="width:100%">
+                    <tr>
+                        <td>ID</td>
+                        <td>Number</td>
+                        <td>Price</td>
+                        <td>Type</td>
+                    </tr>
+                <?php
+                foreach ($result_list_all as $key => $value_list_all) {
+                    echo "<tr>";
+                        echo "<td>" . $value_list_all["id"]. "</td>";
+                        echo "<td>" . $value_list_all["number"]. "</td>";
+                        echo "<td>" . $value_list_all["price"]. "</td>";
+                        echo "<td>" . $value_list_all["type"]. "</td>";
+                    echo "<tr>";
+                }
+                    ?>
+                </table>
+
+            </div>
+
+            <div class="col-sm-4">
+                <h3>Input number</h3>
+                
+                <form method="post" action="./api/post_number.php">
+                    <input type="text" name="number">
+                    <input type="hidden" name="price" value="0">
+                    <input type="hidden" name="type" value="บน">
+
+                    <input type="submit">
+                </form>
+
+            </div>
+
+            <div class="col-sm-4">
+            </div>
+
+        </div>
+    </div>
 
 </body>
 
