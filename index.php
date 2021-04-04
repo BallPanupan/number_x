7dab1c4315c7a943f90d5bd18057af5e26d51419
Ball <ball@reverseads.com>
2021-04-04 14:31:33 +0700

    html

--- index.php
+++ index.php
@@ -2,17 +2,14 @@
 <html>
 
 <head>
-    <title>number_x</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <script src="./jquery/jquery.min.js"></script>
-    <script src="./bootstrap/bootstrap.min.js"></script>
+    <title>Page Title</title>
 </head>
 
 <body>
-    <div class="jumbotron text-center">
-    </div>
+
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+
 </body>
 
 </html>
