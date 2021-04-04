bd094ade17bb5b4201bf43bd7961bfec1f0b4b81
Ball <ball@reverseads.com>
2021-04-04 14:30:20 +0700

    d

--- index.php
+++ index.php
@@ -5,34 +5,14 @@
     <title>number_x</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <script src="./jquery/jquery.min.js"></script>
     <script src="./bootstrap/bootstrap.min.js"></script>
-    
 </head>
 
 <body>
-
     <div class="jumbotron text-center">
-            <h1>number_x</h1>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-4">
-                    </div>
-
-                    <div class="col-sm-4">
-                        <h3>Input number</h3>
-                        <input type="text">
-                    </div>
-
-                    <div class="col-sm-4">
-                    </div>
-
-                </div>
-            </div>
     </div>
-
 </body>
 
 </html>
