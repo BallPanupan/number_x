b4856404a40fe47a1fce5fec4aa284f6ffca5a19
Ball <ball@reverseads.com>
2021-04-04 14:32:06 +0700

    add title

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 <html>
 
 <head>
-    <title>Page Title</title>
+    <title>number_x</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./jquery/jquery.min.js"></script>
+    <script src="./bootstrap/bootstrap.min.js"></script>
 </head>
 
 <body>
